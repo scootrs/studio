a2db5dd25f96f41a25ab9e9450df2708b0c3bfec
Braden Hitchcock <developer.hitchcock@gmail.com>
2020-01-23 08:54:14 -0800

    refactor: update utility bar color

--- src/components/workspace/blueprint/canvas/utility-bar/view.js
+++ src/components/workspace/blueprint/canvas/utility-bar/view.js
@@ -7,7 +7,8 @@ const Bar = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  background-color: ${({ theme }) => theme.colors.backgrounds.light};
+  background-color: ${({ theme }) =>
+    theme.mode === 'light' ? theme.colors.backgrounds.light : theme.colors.backgrounds.medium};
   color: ${({ theme }) => theme.colors.fonts.light};
   min-width: 60px;
   flex-shrink: 0;
