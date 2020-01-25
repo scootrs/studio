ac6ef10416fca41f67bd207f7b1b0612635fcdab
Braden Hitchcock <developer.hitchcock@gmail.com>
2020-01-24 16:51:10 -0800

    fix: hidden resource id with long id

--- src/components/workspace/details-pane/details/view/header.js
+++ src/components/workspace/details-pane/details/view/header.js
@@ -4,9 +4,11 @@ import { ValidatedTextInput } from '~styles/input/text-validated';
 export const HeaderIcon = styled.div``;
 
 export const HeaderTitleContainer = styled.div`
+  flex-grow: 1;
   display: flex;
   flex-direction: column;
   max-width: 100%;
+  margin-right: 10px
 `;
 
 export const HeaderTitleInput = styled(ValidatedTextInput)`
