438a7e46e368ed7068ef6af10fcdc537ae0709b8
Braden Hitchcock <developer.hitchcock@gmail.com>
2020-01-30 09:04:12 -0800

    feat: update python code template

--- src/templates/python.js
+++ src/templates/python.js
@@ -7,7 +7,22 @@ def endpoint(event, context):
     #
     return create_json_response(200, { "message": "Success" })
 
+class CustomJsonEncoder(json.JSONEncoder):
+    """
+    This is a custom class that allows the \`json\` library to encode decimal types without
+    throwing an error. The encoder is used in the \`create_json_response\` method below.
+    """
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            return str(obj)
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
+
 def create_json_response(code, body = {}, headers = {}):
+    """
+    Creates the AWS Lambda response necessary for responding to API Gateway events when
+    the response body is formatted as JSON.
+    """
     original_headers = {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
@@ -17,6 +32,6 @@ def create_json_response(code, body = {}, headers = {}):
     return {
         "statusCode": code,
         "headers": original_headers,
-        "body": json.dumps(body)
+        "body": json.dumps(body, cls=CustomJsonEncoder)
     }
 `;
