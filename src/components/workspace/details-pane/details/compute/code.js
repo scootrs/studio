48455dd29c51e05e3245e6dd60ccd21a8c105fab
Braden Hitchcock <developer.hitchcock@gmail.com>
2020-01-31 14:39:38 -0800

    refactor: improve monaco editor usability

--- src/components/workspace/details-pane/details/compute/code.js
+++ src/components/workspace/details-pane/details/compute/code.js
@@ -1,168 +1,52 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import styled, { withTheme } from 'styled-components';
-import * as monaco from 'monaco-editor';
 import { useWorkspaceContext } from '~contexts/workspace';
+import { useMonaco } from '~hooks/monaco';
 import templates from '~templates';
 import { detectLanguageFromRuntime } from './language';
 
 const CodeDetailsRoot = styled.div`
+  flex-grow: 1;
+  align-self: stretch;
   display: flex;
   flex-direction: column;
-  flex-grow: 1;
-  width: 100%;
-  position: relative;
-  height: 100%;
+  align-items: stretch;
 `;
 
 const Editor = styled.div`
   flex-grow: 1;
+  position: relative;
 `;
 
-function setMonacoLanguageForRuntime(runtime) {
-  const language = detectLanguageFromRuntime(runtime);
-  monaco.editor.setModelLanguage(document.editor.monaco.getModel(), language);
-}
+export function ComputeCodeDetailsPanel({ theme }) {
+  const workspaceContext = useWorkspaceContext();
 
-export function ComputeCodeDetailsPanel({ theme, autosaveDelayMs = 1000 }) {
-  const {
-    state: { selected },
-    actions: { updateResourceConfiguration }
-  } = useWorkspaceContext();
+  const { selected } = workspaceContext.state;
 
-  // Initialize our global instance of the Monaco editor. Only change it if the theme mode changes.
-  //
-  const mode = theme.mode;
-  useEffect(() => {
-    if (!document.editor) {
-      let el = document.createElement('div');
-      el.style.width = '99%';
-      el.style.height = '99%';
-      el.style.position = 'relative';
+  const { updateSelectedConfiguration } = workspaceContext.actions;
 
-      let editor = monaco.editor.create(el, {
-        minimap: {
-          enabled: false
-        }
-      });
+  const { code } = selected.config;
+  const language = detectLanguageFromRuntime(selected.config.runtime);
 
-      document.editor = {
-        id: null,
-        el,
-        monaco: editor
-      };
-    }
+  if (code === '' && language !== 'text') {
+    updateSelectedConfiguration({ code: templates[language] });
+  }
 
-    let monacoTheme = 'vs-light';
-    if (mode === 'dark') {
-      monacoTheme = 'vs-dark';
-    }
-    monaco.editor.setTheme(monacoTheme);
-
-    function resize() {
-      document.editor.monaco.layout();
-    }
-    document.addEventListener('split-resize', resize);
-    return function() {
-      document.removeEventListener('split-resize', resize);
-    };
-  }, [mode]);
-
-  // Create an effect that will update the context handler for updating resource configuration every time the state
-  // changes. Note that state does not change when a user is typing text into the editor itself (reducing renders).
-  // We do, however, set a timeout on every keystroke so that the editor will autosave after the user has stopped typing
-  // for a certain amount of time.
-  const saveActionRegistrationRef = useRef(null);
-  const onKeyupListenerRef = useRef(null);
-  const timeoutSaveRef = useRef(null);
-  const didSaveAfterTimeoutRef = useRef(false);
-  useEffect(() => {
-    saveActionRegistrationRef.current = document.editor.monaco.addAction({
-      id: 'save',
-      label: 'Save',
-      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S],
-      precondition: null,
-      keybindingContext: null,
-      run: function(ed) {
-        updateResourceConfiguration(document.editor.id, { code: ed.getValue() });
-      }
-    });
-    onKeyupListenerRef.current = document.editor.monaco.onKeyUp(function() {
-      if (timeoutSaveRef.current !== null) {
-        clearTimeout(timeoutSaveRef.current);
-      }
-      timeoutSaveRef.current = setTimeout(function() {
-        updateResourceConfiguration(document.editor.id, { code: document.editor.monaco.getValue() });
-        timeoutSaveRef.current = null;
-        didSaveAfterTimeoutRef.current = true;
-      }, autosaveDelayMs);
-    });
-
-    return function() {
-      saveActionRegistrationRef.current.dispose();
-      onKeyupListenerRef.current.dispose();
-      if (timeoutSaveRef.current !== null) {
-        clearTimeout(timeoutSaveRef.current);
-      }
-    };
-  }, [saveActionRegistrationRef, onKeyupListenerRef, timeoutSaveRef, autosaveDelayMs, updateResourceConfiguration]);
-
-  // This effect simply saves what is in the editor into the context state when the component unmounts.
-  //
-  const updateFunctionRef = useRef();
-  updateFunctionRef.current = updateResourceConfiguration;
-  useEffect(
-    function() {
-      return function() {
-        updateFunctionRef.current(document.editor.id, { code: document.editor.monaco.getValue() });
-        // We have to set the editor ID to null here instead of in the first effect so that we don't try to update
-        // with a null ID
-        document.editor.id = null;
-      };
+  const ref = useMonaco('code', {
+    value: code,
+    language: language,
+    theme: theme.mode === 'light' ? 'vs-light' : 'vs-dark',
+    broker: document,
+    autosave: {
+      enable: true,
+      timeout: 800
     },
-    [updateFunctionRef]
-  );
-
-  // This effect sets the value and language of the editor when a new resource is selected. It also catches the edge
-  // case where after an autosave we ignore any potential side-effects in the editor.
-  //
-  const ref = useRef(null);
-  useEffect(() => {
-    if (selected.meta.id !== document.editor.id) {
-      // A new resource has been selected but the editor is still in view. We need to update the editor with the
-      // new code
-      document.editor.monaco.setValue(selected.config.code || '');
-      setMonacoLanguageForRuntime(selected.config.runtime);
-      document.editor.id = selected.meta.id;
-    } else if (didSaveAfterTimeoutRef.current) {
-      // This effect is being triggered after an automatic save of the updated code after the timeout. Nothing else
-      // would have changed, so we don't need to do anything else.
-      didSaveAfterTimeoutRef.current = false;
-    } else {
-      // Other state may have been updated. We still need to show those changes
-    }
-  }, [selected]);
-
-  // If the runtime changes, then we need to update the intellisense
-  //
-  const { runtime } = selected.config;
-  useEffect(() => {
-    setMonacoLanguageForRuntime(runtime);
-    const language = detectLanguageFromRuntime(runtime);
-    if (document.editor.monaco.getValue() === '') {
-      const template = templates[language];
-      if (template) {
-        document.editor.monaco.setValue(template);
+    actions: {
+      onSave: function(val) {
+        updateSelectedConfiguration({ code: val });
       }
     }
-  }, [runtime]);
-
-  // This effect attaches the editor to the DOM
-  //
-  useEffect(() => {
-    ref.current.appendChild(document.editor.el);
-    document.editor.monaco.layout();
-    document.editor.monaco.focus();
-  }, [ref]);
+  });
 
   return (
     <CodeDetailsRoot>
---
+++ src/hooks/monaco.js
@@ -0,0 +1,204 @@
+import { useRef, useEffect } from 'react';
+import * as monaco from 'monaco-editor';
+
+// We define a single monaco instance to handle all of our code
+let _editor = null;
+
+// We also need to define a variable to keep track of the element the editor belongs to. This is a work around the
+// inability to reset the editor DOM node after it has been created.
+let _el = null;
+
+// We will setup an object to hold all of the editor models (this will allow us to later have tabs or maintain undo
+// stacks)
+const _models = {};
+
+// We will also setup an object to keep track of all of the view states for each of our models. View states will be
+// saved immediately before a new model is loaded as the current model
+const _views = {};
+
+// This object defines all of the events that the monoco editor will react to
+//
+const _events = {
+  resize: 'monoco:resize'
+};
+
+// These are the default options for the hook
+const _options = {
+  value: '',
+  language: 'text',
+  theme: 'vs-light',
+  broker: null,
+  autosave: {
+    enable: false,
+    timeout: 1000
+  },
+  actions: {
+    onSave: null
+  }
+};
+
+export function useMonaco(model = 'default', options = {}) {
+  // Merge the provided options with the defaults
+  // TODO: make this a deep merge
+  options = Object.assign(_options, options);
+
+  console.log(options);
+
+  // Create the reference we will use for attaching our editor to the DOM
+  const domElementRef = useRef();
+
+  // Initialize and attach editor to DOM
+  //
+  useEffect(() => {
+    if (_el === null) {
+      _el = document.createElement('div');
+      _el.style.position = 'relative';
+      _el.style.width = '99%';
+      _el.style.height = '99%';
+    }
+    if (_editor === null) {
+      // This is the first time the editor has been needed. We should create the editor.
+      console.log('Initializing monoco editor');
+      _editor = monaco.editor.create(_el, {
+        minimap: {
+          enabled: false
+        }
+      });
+    }
+    // Attach the element to the DOM
+    domElementRef.current.append(_el);
+    _editor.layout();
+  }, [_editor, domElementRef]);
+
+  // Set the theme
+  //
+  useEffect(() => {
+    monaco.editor.setTheme(options.theme);
+  }, [options.theme]);
+
+  // Set the model
+  //
+  // We also keep a reference to the name of the previous model so that we can save the view state when it changes.
+  //
+  const prevModelNameRef = useRef(model);
+  useEffect(() => {
+    if (!_models[model]) {
+      console.log('Creating a new monaco model: ' + model);
+      _models[model] = monaco.editor.createModel();
+    }
+    _views[prevModelNameRef.current] = _editor.saveViewState();
+    _editor.setModel(_models[model]);
+    _editor.restoreViewState(_views[model]);
+    prevModelNameRef.current = model;
+  }, [_editor, _models, model, prevModelNameRef]);
+
+  // Update the current model based on options
+  //
+  useEffect(() => {
+    // Set language
+    //
+    monaco.editor.setModelLanguage(_models[model], options.language);
+
+    // Set value
+    //
+    // We only want to set the value if the value is different than what we already see in the editor.
+    // We have this check in place because the value provided in the options could have changed after a parent
+    // state change, but these state changes are not tracked in the editor (in order to improve performance).
+    //
+    const currentValueIsTheSame = checkCodeContentEquality(options.value, _models[model].getValue());
+    if (!currentValueIsTheSame) {
+      _models[model].setValue(options.value);
+    }
+  }, [_models, model, options.value, options.language]);
+
+  // Setup broker event handlers
+  //
+  useEffect(() => {
+    const broker = options.broker;
+
+    function onResize() {
+      _editor.layout();
+    }
+    broker.addEventListener(_events.resize, onResize);
+
+    return () => {
+      broker.removeEventListener(_events.resize, onResize);
+    };
+  }, [_editor, _events, options.broker]);
+
+  // Register and update actions
+  //
+  const actions = useRef({
+    save: null,
+    autosave: {
+      timeout: null,
+      disposer: null
+    }
+  });
+  useEffect(() => {
+    // Register an action for Ctrl + s (saving)
+    //
+    actions.current.save = _editor.addAction({
+      id: 'save',
+      label: 'Save',
+      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S],
+      precondition: null,
+      keybindingContext: null,
+      run: function() {
+        if (options.actions.onSave) {
+          options.actions.onSave(_editor.getValue());
+        }
+      }
+    });
+
+    // Enable autosave when the user has stopped typing after a certain amount of time
+    //
+    if (options.autosave.enable) {
+      actions.current.autosave.disposer = _editor.onKeyUp(function() {
+        if (actions.current.autosave.timeout) {
+          clearTimeout(actions.current.autosave.timeout);
+        }
+        actions.current.autosave.timeout = setTimeout(function() {
+          if (options.actions.onSave) {
+            options.actions.onSave(_editor.getValue());
+          }
+        }, options.autosave.timeout);
+      });
+    }
+
+    return () => {
+      // Cleanup the Ctrl + s action
+      actions.current.save.dispose();
+
+      // Cleanup the autosave action
+      if (options.autosave.enable) {
+        actions.current.autosave.disposer.dispose();
+        if (actions.current.autosave.timeout) {
+          clearTimeout(actions.current.autosave.timeout);
+        }
+      }
+    };
+  }, [_editor, actions, options.actions.onSave, options.autosave.enable, options.autosave.timeout]);
+
+  return domElementRef;
+}
+
+function checkCodeContentEquality(a, b) {
+  const ah = hashCodeContent(a);
+  const bh = hashCodeContent(b);
+  return ah === bh;
+}
+
+// https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+function hashCodeContent(code) {
+  let hash = 0;
+  let i;
+  let chr;
+  if (code.length === 0) return hash;
+  for (i = 0; i < code.length; i++) {
+    chr = code.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
